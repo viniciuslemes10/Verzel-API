0c1f411623017680bc92c77426813fee11c3c2f6
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-07-31 20:53:20 -0300

    feat: implementando get para buscar por id

--- verzel/src/main/java/com/br/veiculos/verzel/controllers/VeiculosController.java
+++ verzel/src/main/java/com/br/veiculos/verzel/controllers/VeiculosController.java
@@ -5,11 +5,9 @@ import com.br.veiculos.verzel.records.VeiculosDetalhamentoDTO;
 import com.br.veiculos.verzel.services.VeiculosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
@@ -21,7 +19,7 @@ public class VeiculosController {
     @Autowired
     private VeiculosService service;
 
-    @PostMapping(consumes = "multipart/form-data")
+    @PostMapping(consumes = "multipart/form-data", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<VeiculosDetalhamentoDTO> create(
             @RequestParam("nome") String nome,
             @RequestParam("marca") String marca,
@@ -32,5 +30,11 @@ public class VeiculosController {
         VeiculosDTO data = new VeiculosDTO(nome, marca, modelo, valor, foto);
         var veiculoSaved = service.create(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(new VeiculosDetalhamentoDTO(veiculoSaved));
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<VeiculosDetalhamentoDTO> getVeiculosById(@PathVariable("id") Long id) {
+        var veiculoFound = service.findById(id);
+        return ResponseEntity.ok(new VeiculosDetalhamentoDTO(veiculoFound));
     }
 }
