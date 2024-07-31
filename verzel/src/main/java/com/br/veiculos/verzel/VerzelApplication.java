6d6f939fd11ad38f514f4a25b8614e129c629c68
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-07-31 17:23:48 -0300

    Projeto inicial

---
+++ verzel/src/main/java/com/br/veiculos/verzel/VerzelApplication.java
@@ -0,0 +1,13 @@
+package com.br.veiculos.verzel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerzelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerzelApplication.class, args);
+	}
+
+}
---
+++ verzel/src/test/java/com/br/veiculos/verzel/VerzelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.veiculos.verzel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerzelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
