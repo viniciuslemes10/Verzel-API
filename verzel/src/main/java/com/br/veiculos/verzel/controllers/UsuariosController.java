e1edcbae5d858a8b6ed33b993e906d719adbec23
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-08-01 15:24:31 -0300

    feat: Implementando método de create

---
+++ verzel/src/main/java/com/br/veiculos/verzel/controllers/UsuariosController.java
@@ -0,0 +1,31 @@
+package com.br.veiculos.verzel.controllers;
+
+import com.br.veiculos.verzel.records.UsuarioDTO;
+import com.br.veiculos.verzel.records.UsuarioDetalhamentoDTO;
+import com.br.veiculos.verzel.services.UsuarioPermissoesService;
+import com.br.veiculos.verzel.services.UsuariosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("register/users")
+public class UsuariosController {
+    @Autowired
+    private UsuariosService service;
+
+    @Autowired
+    private UsuarioPermissoesService usuarioPermissoesService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UsuarioDetalhamentoDTO> createUser(@RequestBody UsuarioDTO data) {
+        var user = service.createUser(data);
+        usuarioPermissoesService.create(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new UsuarioDetalhamentoDTO(user));
+    }
+}
