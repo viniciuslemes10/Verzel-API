01f9dfdb71b22b12c41831458972f694a5596522
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-08-01 14:14:27 -0300

    feat: implementando método para buscar por email

--- verzel/src/main/java/com/br/veiculos/verzel/repository/UsuarioRepository.java
+++ verzel/src/main/java/com/br/veiculos/verzel/repository/UsuarioRepository.java
@@ -2,8 +2,12 @@ package com.br.veiculos.verzel.repository;
 
 import com.br.veiculos.verzel.model.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuarios, Long> {
+    @Query("SELECT u FROM Usuarios u WHERE u.email = :email")
+    Usuarios findByUserEmail(@Param("email") String email);
 }
