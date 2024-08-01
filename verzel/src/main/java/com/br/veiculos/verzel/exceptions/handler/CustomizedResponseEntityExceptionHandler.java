a9db5524f7bf074547727bf4b602992f608bb7f3
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-08-01 15:22:19 -0300

    feat: implementando exception de permissao nao encontrada

--- verzel/src/main/java/com/br/veiculos/verzel/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ verzel/src/main/java/com/br/veiculos/verzel/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -58,4 +58,13 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(PermissaoNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handlePermissaoNotFoundException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
 }
