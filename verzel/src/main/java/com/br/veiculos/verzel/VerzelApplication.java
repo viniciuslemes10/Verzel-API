9d881c37ab71cd8e31f8c65ec3eb2bf74c5a0038
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-08-02 11:51:07 -0300

    feat: mostrando a senha do usuário adm

--- verzel/src/main/java/com/br/veiculos/verzel/VerzelApplication.java
+++ verzel/src/main/java/com/br/veiculos/verzel/VerzelApplication.java
@@ -2,12 +2,28 @@ package com.br.veiculos.verzel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class VerzelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VerzelApplication.class, args);
+		Map<String, PasswordEncoder> codificar = new HashMap<>();
+		Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder(
+				"", 8, 185000,
+				Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+		codificar.put("pbkdf2", pbkdf2PasswordEncoder);
+
+		DelegatingPasswordEncoder senhaDecodificada = new DelegatingPasswordEncoder("pbkdf2", codificar);
+		senhaDecodificada.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder);
+		String senha = senhaDecodificada.encode("lemes123");
+		System.out.println(senha);
 	}
 
 }
